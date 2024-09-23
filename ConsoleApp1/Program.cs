32ab6b3d3750f85f8e86c3504bf7876040620ef0
sigmotoa <si.galvismotoa@gmail.com>
2024-09-23 15:02:58 -0500

    Ejercicios de inicio

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, Escribe tu nombre!");
+String name;//Declaracion
+name = Console.ReadLine(); //Asignacion
+Console.WriteLine("Hola y Bienvenido "+name+" no olvides que C# es divertido");
+Console.WriteLine("Ingresa tu año de nacimiento");
+int actual = 2024;//Declaración y Asignación
+int nacimiento = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine(2024-nacimiento+" años es lo que tienes ");
+
+
+//Variables
+//Esto no lo va a leer el compilador
+//
+
+///////Tipos de variables
+/*
+ * int
+ * double
+ * char
+ * boolean
+ * float
+ * long
+ * byte
+ * String
+ *
+ *
+ * 
+*/
