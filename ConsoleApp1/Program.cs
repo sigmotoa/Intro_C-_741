59be52c2bee556647d94a7919811e9c9e16c3ebb
sigmotoa <si.galvismotoa@gmail.com>
2024-09-27 15:10:14 -0500

    Ejercicios para decisiones

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, Escribe tu nombre!");
-String name;//Declaracion
+String name; //Declaracion
 name = Console.ReadLine(); //Asignacion
-Console.WriteLine("Hola y Bienvenido "+name+" no olvides que C# es divertido");
+Console.WriteLine("Hola y Bienvenido " + name + " no olvides que C# es divertido");
 Console.WriteLine("Ingresa tu año de nacimiento");
-int actual = 2024;//Declaración y Asignación
+int actual = 2024; //Declaración y Asignación
 int nacimiento = Convert.ToInt16(Console.ReadLine());
-Console.WriteLine(2024-nacimiento+" años es lo que tienes ");
+//Console.WriteLine(2024-nacimiento+" años es lo que tienes ");
 
 
 //Variables
@@ -34,5 +34,74 @@ Console.WriteLine(2024-nacimiento+" años es lo que tienes ");
  * Multiplicar 3 numeros
  * Mostrar la edad con la fecha de nacimiento
  * Mostrar cuantos días faltan para cumplir años
- * 
+ *
+ */
+
+//Decisiones/Bifurcaciones -> IF
+
+int edad = actual - nacimiento;
+/*
+if (edad > 18)
+{
+    Console.WriteLine("Puede entrar al sitio de mayores");
+    
+}
+else
+{
+    if (edad >= 15)
+    {
+        Console.WriteLine("Puede entrar si trae un amigo de 18");
+    }
+    else
+    {
+        Console.WriteLine("No entra");
+    }
+}
 */
+//Switch Case
+
+switch (edad)
+{
+    case (16|17):
+        Console.WriteLine("Traiga un amigo mayor");
+        break;
+    
+    case 18:
+        Console.WriteLine("Bienvenido");
+        break;
+    
+    default:
+        if (edad>18)
+        {
+            Console.WriteLine("Super bienvenido");
+        }
+        else
+        {
+            Console.WriteLine("Para afuera");
+        }
+        break;
+    
+}
+
+
+
+//Ejercicios
+/*
+ *Usar el MOD -> %
+ * 
+ * Validador de numero par
+ * Validador de multiplo de tres
+ * Validador de multiplo de 5
+ *
+ * Validador multiplo de 7
+ * Mostrar los dias de nacido
+ *
+ *
+ * Switch
+ * Recibir fecha nacimiento entregar signo zodiaco
+ *
+ * Recibir fecha nacimiento y mostrar la generación a la que pertenece
+ *
+ * 
+ */
+
