ff1075be3c9f0fc91351b9f7251c5d3eb8eba350
sigmotoa <si.galvismotoa@gmail.com>
2024-09-28 10:15:06 -0500

    Ciclos y ejercicios

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -38,13 +38,13 @@ int nacimiento = Convert.ToInt16(Console.ReadLine());
  */
 
 //Decisiones/Bifurcaciones -> IF
-
-int edad = actual - nacimiento;
+int edad;
+edad = actual - nacimiento;
 /*
 if (edad > 18)
 {
     Console.WriteLine("Puede entrar al sitio de mayores");
-    
+
 }
 else
 {
@@ -57,7 +57,7 @@ else
         Console.WriteLine("No entra");
     }
 }
-*/
+
 //Switch Case
 
 switch (edad)
@@ -65,11 +65,11 @@ switch (edad)
     case (16|17):
         Console.WriteLine("Traiga un amigo mayor");
         break;
-    
+
     case 18:
         Console.WriteLine("Bienvenido");
         break;
-    
+
     default:
         if (edad>18)
         {
@@ -80,15 +80,15 @@ switch (edad)
             Console.WriteLine("Para afuera");
         }
         break;
-    
+
 }
 
-
+*/
 
 //Ejercicios
 /*
  *Usar el MOD -> %
- * 
+ *
  * Validador de numero par
  * Validador de multiplo de tres
  * Validador de multiplo de 5
@@ -104,6 +104,56 @@ switch (edad)
  *
  * Recibir fecha nacimiento y mostrar la generación a la que pertenece
  *
- * 
- */
+ *
 
+//Ciclos o Iteraciones
+
+//Repetir/Mientras/Para
+//Mientras->While
+
+while (edad<18)
+{
+    //edad -> variable de control o de iteracion
+    Console.WriteLine("=========="+actual+"==========");
+    Console.WriteLine(name+" Aún no puede entrar");
+    Console.WriteLine("Venga el otro año");
+    Console.ReadLine();
+    Console.ReadLine();
+    actual++; //actual = actual+1
+    edad = actual - nacimiento;
+
+}
+
+Console.WriteLine("Bienvenido es el año: "+actual);
+
+
+//Otro while
+
+double control = 0;
+var rand = new Random();
+while (control < 4.2)
+{
+    Console.WriteLine("===============");
+    Console.WriteLine(control);
+    control = rand.NextDouble()*5;
+    Console.WriteLine("===============");
+    Console.WriteLine();
+}
+
+//Pedir al usuario por teclado
+//ingresar letras hasta que pulse la "h"
+
+String letra = "";
+while (letra != "h")
+{
+    Console.WriteLine("----------");
+    Console.WriteLine("Pulse una letra");
+    letra = Console.ReadLine();
+}
+*/
+//Ejercicios Ciclos While
+//Mostrar numeros en pantalla que
+//sean divisibles por 3 desde 0 hasta n -> usuario
+//Mostrar la tabla de multiplicar para el numero por el usuario
+//Mostrar el factorial de un numero ingresado por el usuario
+//Pedir las notas de los estudiantes y mostrar el promedio
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab14a994accccb55114ff934f95fdce9029f52f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d02ada22c5a37dab2b9d5d72784b2df3dad4f19")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
